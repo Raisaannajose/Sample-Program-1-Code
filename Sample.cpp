a203af0bc74cdb78832e6d4f6ce99d4f86cdb9bd
Raisaannajose <122696923+Raisaannajose@users.noreply.github.com>
2023-01-24 18:29:39 +0530

    Update Sample.cpp

--- Sample.cpp
+++ Sample.cpp
@@ -3,66 +3,51 @@
 #include<string>
 #include<vector>
 using namespace std;
-float Attribute1[10][3];
 class DataSet{                          //to store the distance and time inputs and the speed calculated
     public:
-    float arr[10][3];
-    void Data(float a[][3]){
-        for(int i=0;i<10;i++)
-        {
-            arr[i][0]=a[i][0];
-            arr[i][1]=a[i][1];
-            arr[i][2]=a[i][2];
-            cout<<arr[i][0]<<":"<<arr[i][1]<<":"<<arr[i][2]<<endl;       //the stored values are displayed
-            
-        }
-    } 
+    float distance,time1,speed;
 };
 class UserInput{
     public:
-    int k=0;
-    float distance,time1;
-    
-    void Method1(){                                       //Method1 to set the 10 user inputs(distance and time) into Attribute1
-        while(k<10){
+    int i;
+    DataSet arrinput[10];    
+    void Method1(){  
+                                //Method1 to set the 10 user inputs(distance and time) into Attribute1
+        for(i = 0; i < 10; i++)
+        {
             cout<<"Enter the distance travelled in km:";
-            cin>>distance;
+            cin>>arrinput[i].distance;
             cout<<"Enter the time taken in hours:";
-            cin>>time1;
-            Attribute1[k][0]=distance;
-            Attribute1[k][1]=time1;
-            if(k==9){
+            cin>>arrinput[i].time1;
+            if(i==9){
                 break;
             }
-            k++;
             cout<<"Add another input\n";
         }
+        
     }
 };
-class Measure
+class Measure 
 {
     public:
-    void MeasureSpeed(){                                      //MeasureSpeed to measure the speed of all user inputs and update it into Attribute1
+    void MeasureSpeed(DataSet o[]){                                      //MeasureSpeed to measure the speed of all user inputs and update it into Attribute1
         for(int i=0;i<10;i++){
-         Attribute1[i][2]= Attribute1[i][0]/Attribute1[i][1];
+         o[i].speed=o[i].distance/o[i].time1;
         }
      }
 };
-class ShowResults
+class ShowResults 
 {
     public:
-    void WriteToFile()                                     //WriteToFile() to print distance,time and calculated speed from Attribute1 to a csv file
+    void WriteToFile(DataSet arr[])                                     //WriteToFile() to print distance,time and calculated speed from Attribute1 to a csv file
     {
-        ofstream myfile("sample.csv",ios::out|ios::app);   //output in "sample.csv"
+        ofstream myfile("output.csv",ios::out|ios::app);   //output in "sample.csv"
         if(myfile.is_open()){
             for(int i=0;i<10;i++){
-                for(int j=0;j<3;j++){
-                 if(j==2){
-                   myfile<<Attribute1[i][j]; 
-                 }
-                 else{
-                 myfile<<Attribute1[i][j]<<",";
-                 }
+                {
+                    myfile<<arr[i].distance<<",";
+                    myfile<<arr[i].time1<<",";
+                    myfile<<arr[i].speed;
                 }
             myfile<<"\n";
             }
@@ -76,10 +61,8 @@ int main(){
     UserInput obj;
     obj.Method1();
     Measure obj1;
-    obj1.MeasureSpeed();
-    DataSet obj2;
-    obj2.Data(Attribute1);                 //to store the values in DataSet class
+    obj1.MeasureSpeed(obj.arrinput);
     ShowResults obj3;
-    obj3.WriteToFile();
+    obj3.WriteToFile(obj.arrinput);
     return 0;
 }
